c16da3091671de45fa83e9bc8f65cb12fcd12b07
josemanuel00608 <josemanuelloayzavaca7@gmail.com>
2025-10-18 13:21:06 -0400

    Updated 20251015145916_add_roles_and_course_materials.sql

--- supabase/migrations/20251015145916_add_roles_and_course_materials.sql
+++ supabase/migrations/20251015145916_add_roles_and_course_materials.sql
@@ -225,10 +225,10 @@ BEGIN
   IF NOT EXISTS (
     SELECT 1 FROM pg_trigger WHERE tgname = 'on_auth_user_created_role'
   ) THEN
-    CREATE TRIGGER on_auth_user_created_role
-      AFTER INSERT ON auth.users
-      FOR EACH ROW
-      EXECUTE FUNCTION create_user_role();
+   CREATE TRIGGER trg_user_roles_updated_at
+  BEFORE UPDATE ON user_roles
+  FOR EACH ROW
+  EXECUTE FUNCTION update_timestamp();
   END IF;
 END $$;
 
